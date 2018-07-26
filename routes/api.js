eee8e4ae0abdfdfb2a63e20f4acfa7c9bc0a6835
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-07-26 13:17:09 -0700

    post routes updated

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,101 @@
 const router = require("express").Router();
-var db = require("./models");
+var db = require("../models");
 
 
 router.post("/new/teacher", (req, res)=>{
-    db.Teacher.create()
+    db.Teacher.create(req.body).then(function(result){
+        res.json(result);
+    });
 })
 
-router.post("/new/classroom", (req, res)=>{
+router.post("/new/:teacher/classroom", (req, res)=>{
+    db.Classroom.create(req.body).then(function(dbClassroom) {
 
+        return db.Teacher.findOneAndUpdate({}, { $push: { classrooms: dbClassroom._id } }, { new: true });
+
+    }).then(function(dbClassroom) {
+
+        res.json(dbClassroom);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
 router.post("/new/student", (req, res)=>{
+    db.Student.create(req.body).then(function(dbStudent) {
 
+        return db.Classroom.findOneAndUpdate({}, { $push: { students: dbStudent._id } }, { new: true });
+
+    }).then(function(dbClassroom) {
+
+        res.json(dbClassroom);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
-router.post("/new/unit", (req, res) =>{
-    Unit
+router.post("/new/:classroom/unit", (req, res) =>{
+    db.Unit.create(req.body).then(function(dbUnit) {
+
+        return db.Classroom.findOneAndUpdate({}, { $push: { units: dbUnit._id } }, { new: true });
+
+    }).then(function(dbClassroom) {
+
+        res.json(dbClassroom);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
 router.post("/new/note", (req, res)=>{
+    db.Note.create(req.body).then(function(dbNote) {
 
+        return db.Unit.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+
+    }).then(function(dbUnit) {
+
+        res.json(dbUnit);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
 router.post("/new/post", (req, res) =>{
+    db.Post.create(req.body).then(function(dbPost) {
 
+        return db.Unit.findOneAndUpdate({}, { $push: { posts: dbPost._id } }, { new: true });
+
+    }).then(function(dbUnit) {
+
+        res.json(dbUnit);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
 router.post("/new/comment", (req, res)=>{
+    db.Response.create(req.body).then(function(dbResponse) {
 
+        return db.Post.findOneAndUpdate({}, { $push: { responses: dbResponse._id } }, { new: true });
+
+    }).then(function(dbPost) {
+
+        res.json(dbPost);
+
+    }).catch(function(err) {
+
+        res.json(err);
+    });
 });
 
 module.exports = router;
