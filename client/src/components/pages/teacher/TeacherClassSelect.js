3313100ac734963fcaa9e8647d2e924c1f9436e6
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-07-27 14:33:23 -0700

    added redirection

--- client/src/components/pages/teacher/TeacherClassSelect.js
+++ client/src/components/pages/teacher/TeacherClassSelect.js
@@ -10,8 +10,14 @@ class TeacherClassSelect extends React.Component {
 
     componentDidMount(){
         axios.get("/test123").then(res=>{
-            console.log(res);
-        })
+            console.log(res.data.user);
+            if(res.data.user===undefined){
+                console.log("Not logged in, redirecting.");
+                this.props.history.push("/teacherlogin")
+            }else{
+                console.log("login successful");
+            }
+        });
     }
     // Get request to teacher model to return list of classes
 
